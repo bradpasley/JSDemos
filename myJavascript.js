c1543e0c43cba2a82bdd3e3c8d8beeaacc642be6
bradpasley <33439538+bradpasley@users.noreply.github.com>
2021-08-12 10:29:03 +1000

    jQuery theme change

--- myJavascript.js
+++ myJavascript.js
@@ -1,7 +1,8 @@
 
 let mainmenu = {title: "Main Menu", subtitle:"", text:"Welcome to Brad's Javascript demos", button1Text:"", button2Text:""};
 let countmenu = {title: "Counter", subtitle:"Count", text:"Push the button to add/subtract", button1Text:"Add", button2Text:"Subtract", value:0};
-let darklightmenu = {title: "Theme", subtitle:"Light Mode", text:"Push the button switch between Light Mode and Dark Mode", button1Text:"Light", button2Text:"Dark", value:"light"};
+let darklightmenu = { title: "Theme", subtitle: "Light Mode", text: "Push the button switch between Light Mode and Dark Mode", button1Text: "Light", button2Text: "Dark", value: "light" };
+let colourmenu = { title: "Colours", subtitle: "Colour Array", text: "Push the buttons to switch colours", button1Text: "Lighter", button2Text: "Darker", value: "777777" };
 let pages = {mainmenu, countmenu, darklightmenu};
 
 function displayMenu(menu_title) {
@@ -11,14 +12,20 @@ function displayMenu(menu_title) {
             menu_object = mainmenu;
             document.getElementById("cardButton1").innerHTML = "";
             document.getElementById("cardButton2").innerHTML = "";
+            document.getElementById("cardButton1").classList.add("invisible");
+            document.getElementById("cardButton2").classList.add("invisible");
             break;
         case (mainmenu.title):
             menu_object = mainmenu;
             document.getElementById("cardButton1").innerHTML = "";
             document.getElementById("cardButton2").innerHTML = "";
+            document.getElementById("cardButton1").classList.add("invisible");
+            document.getElementById("cardButton2").classList.add("invisible");
             break;
         case (countmenu.title):
             menu_object = countmenu;
+            document.getElementById("cardButton1").classList.remove("invisible");
+            document.getElementById("cardButton2").classList.remove("invisible");
             document.getElementById("menuTitle").innerHTML = menu_object.subtitle + ": "+menu_object.value;
             document.getElementById("cardButton1").innerHTML = menu_object.button1Text;
             document.getElementById("cardButton1").setAttribute("onclick", "countAdd()");
@@ -28,6 +35,8 @@ function displayMenu(menu_title) {
         case (darklightmenu.title):
             menu_object = darklightmenu;
             document.getElementById("cardButton2").innerHTML = "";
+            document.getElementById("cardButton2").classList.add("invisible");
+            document.getElementById("cardButton1").classList.remove("invisible");
             document.getElementById("cardButton1").setAttribute("onclick", "switchTheme()");
             if(darklightmenu.value == "light") {//if currently "light mode" display "Dark"
                 document.getElementById("cardButton1").innerHTML = darklightmenu.button2Text;
@@ -55,50 +64,33 @@ function countSubtract() {
 }
 
 function switchTheme() {
+    document.getElementById("menuTitle").innerHTML = darklightmenu.value;
     if(darklightmenu.value=="light") {//change to dark
         darklightmenu.value = "dark";
         document.getElementById("cardButton1").innerHTML = darklightmenu.button1Text;
-        document.body.classList.remove("bg-dark");
-        document.body.classList.add("bg-secondary");
-        document.getElementById("centreBackground").classList.remove("bg-info");
-        document.getElementById("centreBackground").classList.add("bg-dark");
-        
-        document.getElementById("mainButton").classList.remove("bg-light");
-        document.getElementById("mainButton").classList.add("bg-dark");
-        document.getElementById("mainButton").classList.remove("text-dark");
-        document.getElementById("mainButton").classList.add("text-light");
 
-        document.getElementById("countButton").classList.remove("bg-light");
-        document.getElementById("countButton").classList.add("bg-success");
-        document.getElementById("countButton").classList.remove("text-success");
-        document.getElementById("countButton").classList.add("text-light");
-        
-        document.getElementById("themeButton").classList.remove("bg-light");
-        document.getElementById("themeButton").classList.add("bg-danger");
-        document.getElementById("themeButton").classList.remove("text-danger");
-        document.getElementById("themeButton").classList.add("text-light");
+        $('.light-green').addClass('dark-green').removeClass('light-green');
+        $('.light-blue').addClass('dark-blue').removeClass('light-blue');
+        $('.light-red').addClass('dark-red').removeClass('light-red');
+        $('.light-bw').addClass('dark-bw').removeClass('light-bw');
+        $('.light-body').addClass('dark-body').removeClass('light-body');
     }
     else {//change to light
         darklightmenu.value = "light";
         document.getElementById("cardButton1").innerHTML = darklightmenu.button2Text;
-        document.body.classList.add("bg-dark");
-        document.body.classList.remove("bg-secondary");
-        document.getElementById("centreBackground").classList.add("bg-info");
-        document.getElementById("centreBackground").classList.remove("bg-dark");
 
-        document.getElementById("mainButton").classList.add("bg-light");
-        document.getElementById("mainButton").classList.remove("bg-dark");
-        document.getElementById("mainButton").classList.add("text-dark");
-        document.getElementById("mainButton").classList.remove("text-light");
-
-        document.getElementById("countButton").classList.add("bg-light");
-        document.getElementById("countButton").classList.remove("bg-success");
-        document.getElementById("countButton").classList.add("text-success");
-        document.getElementById("countButton").classList.remove("text-light");
-        
-        document.getElementById("themeButton").classList.add("bg-light");
-        document.getElementById("themeButton").classList.remove("bg-danger");
-        document.getElementById("themeButton").classList.add("text-danger");
-        document.getElementById("themeButton").classList.remove("text-light");
+        $('.dark-green').addClass('light-green').removeClass('dark-green');
+        $('.dark-blue').addClass('light-blue').removeClass('dark-blue');
+        $('.dark-red').addClass('light-red').removeClass('dark-red');
+        $('.dark-bw').addClass('light-bw').removeClass('dark-bw');
+        $('.dark-body').addClass('light-body').removeClass('dark-body');
     }
+}
+
+function lighterColour() {
+    
+}
+
+function darkerColour() {
+
 }
